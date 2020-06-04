dea29a00f7881732e72b8fc4ca94b8588ef28e46
Samuel2799 <samueltiang2799@gmail.com>
2020-06-04 12:55:33 +0800

    Makenewobstacletest

---
+++ MakeNewObstacleTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MakeNewObstacleTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
--- SnakeTest/UnitTest1.cs
+++ SnakeTest/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using Snake;
 
 namespace SnakeTest
@@ -12,6 +13,20 @@ namespace SnakeTest
             Program.Position position = Program.newRandomPosition(100, 100);
             Program.Position newPosition = Program.newRandomPosition(100, 100); 
             Assert.AreNotEqual(position.col, newPosition.col);
+        }
+
+        [TestMethod]
+        public void makeNewObstacleTest()
+        {
+            List<Program.Position> obstacles = new List<Program.Position>
+            {       Program.newRandomPosition(22, 40),
+                    Program.newRandomPosition(70, 100),
+                    Program.newRandomPosition(95, 60),
+                    Program.newRandomPosition(120, 80),
+                    Program.newRandomPosition(200, 130),};
+            Program.Position obstacle = Program.newRandomPosition(56, 150);
+            obstacles.Add(obstacle);
+            Assert.AreEqual(6, obstacles.Count);
         }
     }
 }
